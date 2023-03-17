35fda7a94667daa4888a08a5d630c01a08ad8680
Alexander Rulyov <rulev@citylink.pro>
2023-03-17 13:08:16 +0300

    обновлен env example; обновлен composer.json; добавлен Enum для способов генерации токенов ссылок

---
+++ app/Services/Link/Enums/TokenGenerateMethod.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\Link\Enums;
+
+enum TokenGenerateMethod
+{
+    case simpleEnum;
+    case simpleShuffle;
+}
--- app/Services/TokenGenerator.php
+++ app/Services/TokenGenerator.php
@@ -2,10 +2,11 @@
 
 namespace App\Services;
 
+use App\Services\Link\Enums\TokenGenerateMethod;
+
 class TokenGenerator
 {
-//    private const PERMITTED_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    private const PERMITTED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    private const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     private function __construct() {}
     private function __clone() {}
@@ -14,16 +15,30 @@ class TokenGenerator
      * @param int $strength
      * @return string
      */
-    public static function generate(int $strength = 7): string
+    public static function generate(int $strength = 7, TokenGenerateMethod $method = TokenGenerateMethod::simpleShuffle): string
     {
-        $length = strlen(self::PERMITTED_CHARS);
+        return match ($method) {
+            TokenGenerateMethod::simpleShuffle => self::simpleShuffle($strength),
+            TokenGenerateMethod::simpleEnum => self::simpleEnum($strength)
+        };
+    }
+
+    private static function simpleEnum(int $strength = 7): string
+    {
+        $length = strlen(self::ALPHABET);
         $randomToken = '';
 
         for($i = 0; $i < $strength; $i++) {
-            $randomCharacter = self::PERMITTED_CHARS[mt_rand(0, $length - 1)];
-            $randomToken .= $randomCharacter;
+            $randomToken .= self::ALPHABET[mt_rand(0, $length - 1)];
         }
 
         return $randomToken;
     }
+
+    private static function simpleShuffle(int $strength = 7): string
+    {
+        return substr(str_shuffle(self::ALPHABET), 0, $strength);
+    }
 }
+
+
